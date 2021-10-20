cae2ae8b4d52d48c1cd622be1b9ecee1386bc47f
Daniel Alvarado <danieltrm1810@gmail.com>
2021-10-19 23:20:35 -0300

    se agregan estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,58 @@
-import { useState } from 'react';
-import './App.css';
-import {defaultState} from "./products.js";
-import {ObtenerItemsPorCategoria} from "./utils.js"
-import Items from './Items';
+import { useState } from "react";
+import "./App.css";
+import { defaultState } from "./products.js";
+import { ObtenerItemsPorCategoria } from "./utils.js";
+import Items from "./Items";
+import Jetsetter from "./svg/Jetsetter.svg";
 
 function App() {
-
   const [state, setState] = useState("");
 
   const [lista, setLista] = useState(defaultState);
 
   const manejadorAgregarItem = (e) => {
     return setState(e.target.value);
-  }
+  };
 
   const manejadorSubmit = (e) => {
     return console.log(e.target.value);
-  }
+  };
 
-  const listaDesempacados = ObtenerItemsPorCategoria(lista, "packed", false)
-  const listaEmpacados = ObtenerItemsPorCategoria(lista, "packed", true)
-
+  const listaDesempacados = ObtenerItemsPorCategoria(lista, "packed", false);
+  const listaEmpacados = ObtenerItemsPorCategoria(lista, "packed", true);
 
   return (
     <div className="App">
+      <img height="250px" width="300px" src={Jetsetter} alt="Logo" />
       <div className="Container">
-        <form>
+        <form className="formTop">
           <label>
-          <input onChange={manejadorAgregarItem} type="text" value={state} />
+            <input
+              className="input-top"
+              onChange={manejadorAgregarItem}
+              type="text"
+              value={state}
+            />
           </label>
-          <input onClick={manejadorSubmit} type="submit" value="Agregar item" />
+          <input
+            className="submit-top"
+            onClick={manejadorSubmit}
+            type="submit"
+            value="Agregar item"
+          />
         </form>
-        <Items title="Items no empacados" items={listaDesempacados} lista={lista} setLista={setLista} />
-        <Items title="Items empacados" items={listaEmpacados} lista={lista} setLista={setLista} />
+        <Items
+          title="Items no empacados"
+          items={listaDesempacados}
+          lista={lista}
+          setLista={setLista}
+        />
+        <Items
+          title="Items empacados"
+          items={listaEmpacados}
+          lista={lista}
+          setLista={setLista}
+        />
         <div>
           <button> Marcar todo desempacado </button>
         </div>
--- src/Items.jsx
+++ src/Items.jsx
@@ -1,32 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
-export default function Items (props) {
-    const [inputState, setInputState] = useState("");
+export default function Items(props) {
+  const [inputState, setInputState] = useState("");
 
-    const manejadorInput = (e) => {
-        setInputState(e.target.value);
-    }
+  const manejadorInput = (e) => {
+    setInputState(e.target.value);
+  };
 
-    let listaFiltrada = props.items;
+  let listaFiltrada = props.items;
 
-    if (inputState.lenght > 0) {
-        listaFiltrada = props.items.filter((i) => {
-            if(i.value.toUpperCase().includes(inputState.toLocaleUpperCase())){
-               return true;
-            }
-        });
-    }
+  if (inputState.lenght > 0) {
+    // eslint-disable-next-line array-callback-return
+    listaFiltrada = props.items.filter((i) => {
+      if (i.value.toUpperCase().includes(inputState.toLocaleUpperCase())) {
+        return true;
+      }
+    });
+  }
 
-    return(
-        <div className="ItemsContainer">
-            <h1> {props.title}{props.items.lenght} </h1>
-            <input onchange={manejadorInput} type="text" value={inputState} />
-            {listaFiltrada.map ((item) =>{
-                return (
-                    <Item nombre={item.value} id={item.id} packed={item.packed} lista={props.lista} setLista={props.setLista} />
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="ItemsContainer">
+      <h1 className="ItemsTitle">
+        {props.title}
+        {props.items.lenght}{" "}
+      </h1>
+      <input
+        className="input-items"
+        onchange={manejadorInput}
+        type="text"
+        value={inputState}
+        placeholder="buscar"
+      />
+      {listaFiltrada.map((item) => {
+        return (
+          <Item
+            nombre={item.value}
+            id={item.id}
+            packed={item.packed}
+            lista={props.lista}
+            setLista={props.setLista}
+          />
+        );
+      })}
+    </div>
+  );
 }
